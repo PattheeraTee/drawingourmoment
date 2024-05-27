d48c6b621d135654e0e90275288e91aed494bb0b
PattheeraTee <pattheera.mwny@gmail.com>
2024-05-28 00:22:04 +0700

    save project

---
+++ index.js
@@ -0,0 +1,20 @@
+let clickTime = document.getElementById("kazama")
+let clickOther = document.getElementById("nene")
+let clickMoment = document.getElementById("bojung")
+let clickMessage = document.getElementById("chinjung")
+let clickPlaylist = document.getElementById("mazao")
+clickTime.addEventListener('click',function(){
+    window.location.href = './time.html'
+});
+clickMoment.addEventListener('click',function(){
+    window.location.href = './moment.html'
+});
+clickMessage.addEventListener('click',function(){
+    window.location.href = 'https://gifft.me/l#mTUaCPRe88126uApFPTl'
+});
+clickPlaylist.addEventListener('click',function(){
+    window.location.href = './playlist.html'
+});
+clickOther.addEventListener('click',function(){
+    window.location.href = 'https://happyaniversarykrab.netlify.app/'
+});
